ed99d800c2f005959ba7c2c0f4f5b4d92c3c0661
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2024-12-01 13:42:46 +0300

    added cards from array

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
@@ -7,3 +8,11 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((el) => {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = el.link;
+    cardElement.querySelector('.card__title').textContent = el.name;
+
+    cardsList.append(cardElement);
+})
