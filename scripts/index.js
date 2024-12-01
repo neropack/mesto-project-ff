32a82b9503503c9260689f0949ba955c113dfe54
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2024-12-01 14:53:04 +0300

    changed add card behaviour

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,22 @@
 const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardsList = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
 
 // @todo: Функция создания карточки
+function addCard (data) {
+    // переменные: клонирование шаблона и получение кнопки удаления
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
+    // передача значений в шаблон из cards.js
+    cardElement.querySelector('.card__image').src = data.link;
+    cardElement.querySelector('.card__title').textContent = data.name;
+
+    // удаление элемента при нажатии
+    deleteButton.addEventListener('click', deleteCard);
+    return cardElement;
+}
 // @todo: Функция удаления карточки
 function deleteCard (event) {
     event.currentTarget.parentNode.remove();
@@ -12,17 +25,7 @@ function deleteCard (event) {
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((el) => {
-    // переменные: клонирование шаблона и получение кнопки удаления
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-
-    // удаление элемента при нажатии
-    deleteButton.addEventListener('click', deleteCard);
-
-    // передача значений в шаблон из cards.js
-    cardElement.querySelector('.card__image').src = el.link;
-    cardElement.querySelector('.card__title').textContent = el.name;
-
-    // добавление заполненного шаблона в список
-    cardsList.append(cardElement);
-})
+    //добавление заполненного шаблона в список
+    const card = addCard(el);
+    cardsList.append(card);
+});
