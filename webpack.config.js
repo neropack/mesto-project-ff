12c877803d6d138c90f108dea1bdbf7e3e542086
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-01-06 19:29:14 +0300

    webpack-images-fonts

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,11 @@ module.exports = {
             use: 'babel-loader',
             // исключает папку node_modules, файлы в ней обрабатывать не нужно
             exclude: '/node_modules/'
+          },
+          {
+            // регулярное выражение, которое ищет все файлы с такими расширениями
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource'
           }
           ]
       },
